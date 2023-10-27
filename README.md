11fda142947f4ba857ad4556940698436150530e
jamesii-b <jamesbhattarai14@gmail.com>
2023-10-27 13:50:09 +0545

    Fix: Time error on salesgraph data && modify readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Nodejs-ERP
+
+This is a backend system built for the frontend of ERP and POS system. 
+
+
+## Routes
+
+
+### Product Routes
+`/add-product` <br>
+`/sales`
+`/sales/division` | Eg : ```/sales/pName=Marshmello``` <br>
+`/sales/division?t=2023_10_20` ||  ```/sales/division? t=2023_10_20to2023_10_30``` -> Optional <br>
+`/checkout`
+
+
+
+### Graph Dataum & Widgests Routes
+
+`/salesgraph` <br>
+`/salesgraph/divison` | Eg: ```/salesgraph/pName=Marshmello```
+
+
+## Individual Route Definition
+
+### `Checkout`
+It accepts the json as <br>
+Data=[<br>
+"1698000485604","1698000485604" <br>
+]
+<br> Here these datum are individual product id.
--- controller/allGraphData.js
+++ controller/allGraphData.js
@@ -5,13 +5,25 @@ const calculateData = require("../function/salesGraph.js")
 async function allSalesGraphData(req, res) {
 
     const query = req.query.t
-    try {
+    if (query) {
 
-        resSales = await axios.get("http://localhost:5000/sales?t=" + query)
-        var calculatedData = await calculateData(resSales.data)
-        res.json(calculatedData);
-    } catch (err) {
-        res.json("Internal Server Error")
+        try {
+
+            resSales = await axios.get("http://localhost:5000/sales?t=" + query)
+            var calculatedData = await calculateData(resSales.data)
+            res.json(calculatedData);
+        } catch (err) {
+            res.json("Internal Server Error")
+        }
+    } else {
+        try {
+
+            resSales = await axios.get("http://localhost:5000/sales")
+            var calculatedData = await calculateData(resSales.data)
+            res.json(calculatedData);
+        } catch (err) {
+            res.json("Internal Server Error")
+        }
     }
 
 }
