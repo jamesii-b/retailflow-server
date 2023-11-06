0772717e229a4250469000b04f5d0066cd983c79
jamesii-b <jamesbhattarai14@gmail.com>
2023-11-06 21:15:18 +0545

    Feat: Added Supplier search

--- routes/supplierRoute.js
+++ routes/supplierRoute.js
@@ -1,6 +1,49 @@
+const ProductItem = require("../models/productItem");
+const Supplier = require("../models/supplier");
+const express = require("express");
 const router = express.Router();
 
+router.get("/supplier/search/:sName?", async (req, res) => {
 
-router.get()
+    const searchQuery = req.params.sName;
+    console.log("sName");
+    if (!searchQuery) {
+        const suppliers = await Supplier.find({});
+        return res.json({ success: true, suppliers });
+    }
+    const query = new RegExp("[a-zA-Z0-9]*" + searchQuery + "[a-zA-Z0-9]*", "i");
+    const suppliers = await Supplier.find({
+        $or: [
+            { sName: { $regex: query } },
+            { sAddress: { $regex: query } },
+            { sContactNo: { $regex: query } },
+            { sEmail: { $regex: query } },
+        ],
+
+
+    });
+    return res.json({ success: true, suppliers });
+})
+
+router.post("/add-supplier", async (req, res) => {
+    // const { sName, sAddress, sContactNo, sEmail } = req.body;
+    // if (!sName || !sAddress || !sContactNo || !sEmail) {
+    //     return res.status(400).json({ msg: "Please enter all fields" });
+    // }
+    try {
+        const newSupplier = new Supplier({
+            sName: req.body.sName,
+            sAddress: req.body.sAddress,
+            sContactNo: req.body.sContactNo,
+            sEmail: req.body.sEmail,
+        });
+        await newSupplier.save();
+        res.json({ msg: "Supplier added successfully" });
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ msg: "Internal Server Error" });
+    }
+});
+
 
 module.exports = router;
